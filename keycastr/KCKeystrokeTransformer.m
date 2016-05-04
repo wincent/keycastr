100230f3a9a10554f218977b6e9ce9e872b9ce07
Greg Hurrell <greg@hurrell.net>
2016-05-04 01:03:38 -0700

    Make things work on Colemak
    This is a gross hack.
    
    - Xcode screwed up spaces vs tabs and indentation, and I can't be bothered
      fixing it.
    - Hacked the two problematic keys for Colemak (: and o, which got mismatched).
    
    Real fix hopefully forthcoming via:
    
    https://github.com/keycastr/keycastr/issues/56

--- keycastr/KCKeystrokeTransformer.m
+++ keycastr/KCKeystrokeTransformer.m
@@ -83,6 +83,30 @@ static NSString* kShiftKeyString = nil;
 	{
 		d = [[NSDictionary alloc] initWithObjectsAndKeys:
 			UTF8("\xe2\x87\xa4"), NSNum(48), // tab
+            UTF8("\x7E"), NSNum(50), // ~
+            UTF8("\x5f"), NSNum(27), // _
+            UTF8("\x2b"), NSNum(24), // +
+            UTF8("\x7b"), NSNum(33), // {
+            UTF8("\x7d"), NSNum(30), // }
+
+            //UTF8("\x3a"), NSNum(41), // :
+            UTF8("\x3a"), NSNum(35), // :
+
+            UTF8("\x22"), NSNum(39), // "
+            UTF8("\x3c"), NSNum(43), // <
+            UTF8("\x3e"), NSNum(47), // >
+            UTF8("\x3f"), NSNum(44), // ?
+            UTF8("\x7c"), NSNum(42), // |
+            UTF8("\x29"), NSNum(29), // )
+            UTF8("\x21"), NSNum(18), // !
+            UTF8("\x40"), NSNum(19), // @
+            UTF8("\x23"), NSNum(20), // #
+            UTF8("\x24"), NSNum(21), // $
+            UTF8("\x25"), NSNum(23), // %
+            UTF8("\x5e"), NSNum(22), // ^
+            UTF8("\x26"), NSNum(26), // &
+            UTF8("\x2a"), NSNum(28), // *
+            UTF8("\x28"), NSNum(25), // (
 			nil];
 	}
 	return d;
@@ -140,9 +164,8 @@ static NSString* kShiftKeyString = nil;
 	uint32_t _modifiers = [v modifiers];
 	uint16_t _keyCode = [v keyCode];
 	uint16_t _charCode = [v charCode];
-	
-	BOOL isShifted = NO;
-	BOOL needsShiftGlyph = NO;
+
+    BOOL isShifted = NO;
 	BOOL isCommand = NO;
 
 	if (_modifiers & NSControlKeyMask)
@@ -155,40 +178,29 @@ static NSString* kShiftKeyString = nil;
 		isCommand = YES;
 		[s appendString:kAltKeyString];
 	}
+    if (_modifiers & NSCommandKeyMask)
+    {
+        isCommand = YES;
+        [s appendString:kCommandKeyString];
+    }
 	if (_modifiers & NSShiftKeyMask)
 	{
-		isShifted = YES;
+        isShifted = YES;
 		if (isCommand)
-			[s appendString:kShiftKeyString];
-		else
-			needsShiftGlyph = YES;
-	}
-	if (_modifiers & NSCommandKeyMask)
-	{
-		if (needsShiftGlyph)
-		{
-			[s appendString:kShiftKeyString];
-			needsShiftGlyph = NO;
-		}
-		isCommand = YES;
-		[s appendString:kCommandKeyString];
-	}
-
-	if (isShifted)
-	{
-		id tmp = [[self _shiftedSpecialKeys] objectForKey:NSNum(_keyCode)];
-		if (tmp != nil)
-		{
-			[s appendString:tmp];
-			return s;
-		}
+            [s insertString:kShiftKeyString atIndex:0];
+        id tmp = [[self _shiftedSpecialKeys] objectForKey:NSNum(_keyCode)];
+        if (tmp != nil)
+        {
+            [s appendString:tmp];
+            return s;
+        }
 	}
 
 	id tmp = [[self _specialKeys] objectForKey:NSNum(_keyCode)];
 	if (tmp != nil)
 	{
-		if (needsShiftGlyph)
-			[s appendString:[NSString stringWithUTF8String:"\xe2\x87\xa7"]];
+		if (isShifted)
+			[s appendString:kShiftKeyString];
 		[s appendString:tmp];
 
 		return s;
@@ -197,7 +209,7 @@ static NSString* kShiftKeyString = nil;
 	[s appendString:[NSString stringWithCharacters:&_charCode length:1]];
 
 	// If this is a command string, put it in uppercase.
-	if (isCommand)
+	if (isCommand || isShifted)
 	{
 		NSMutableString *t = [[s uppercaseString] mutableCopy];
         s = [t autorelease];
